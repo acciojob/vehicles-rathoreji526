8b240b014589d2fc2d8bacec98733ae222f068cb
Kamal Rathore <mr.rathoreji52@gmail.com>
2025-11-03 10:26:56 +0530

    some minor changes in Car , Vehicle and F1 class

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -73,17 +73,17 @@ public class Car extends Vehicle {
         this.isManual = isManual;
         this.type = type;
         this.seats = seats;
-
+        this.currentGear = 1;
         //Hint: Car extends Vehicle
     }
 
     public void changeGear(int newGear){
-
+        this.currentGear = newGear;
         System.out.println("changeGear method called - The gear is changed to: " + currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-
+        move(newSpeed , newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
     }
 }
--- src/main/java/com/driver/Vehicle.java
+++ src/main/java/com/driver/Vehicle.java
@@ -38,7 +38,7 @@ public class Vehicle {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 }
